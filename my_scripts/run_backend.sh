e635b927ce63dc01f5ffd1696584a445a9a912f9
Ethan Liu <ethanliu@Ethans-Laptop.local>
2025-04-21 22:36:52 +0800

    Update Scripts

--- my_scripts/run_backend.sh
+++ my_scripts/run_backend.sh
@@ -1,4 +1,5 @@
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 cd "${SCRIPT_DIR}/../backend" || exit
+docker-compose up -d || exit
 source venv/bin/activate
 uvicorn main:app --host 0.0.0.0 --port 8000 
--- my_scripts/run_frontend.sh
+++ my_scripts/run_frontend.sh
@@ -1,3 +1,4 @@
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 cd "${SCRIPT_DIR}/../frontend" || exit
+pnpm install
 pnpm run dev
